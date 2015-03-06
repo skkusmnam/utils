3ef46c74c74e29663eafe282312377ed23495cfb
garammj <garammj@skku.edu>
2015-03-06 15:57:32 +0900

    수정

--- DEVS_Setup/DEVS_Setup/DEVS_Setup/F_MAIN.Designer.cs
+++ DEVS_Setup/DEVS_Setup/DEVS_Setup/F_MAIN.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SES Editor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "DEVS ObjectC";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "DEVS Diagram Display";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(594, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(614, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "추가";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(614, 86);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "추가";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(14, 86);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(594, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(614, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "추가";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(14, 136);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(594, 21);
+            this.textBox3.TabIndex = 7;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(721, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Setup";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 192);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(808, 22);
+            this.statusStrip1.TabIndex = 10;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(59, 17);
+            this.toolStripStatusLabel1.Text = "현재 상태";
+            // 
+            // F_MAIN
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(808, 214);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "F_MAIN";
+            this.ShowIcon = false;
+            this.Text = "DEVS_Setup";
+            this.Load += new System.EventHandler(this.F_MAIN_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
 
--- DEVS_Setup/DEVS_Setup/DEVS_Setup/F_MAIN.cs
+++ DEVS_Setup/DEVS_Setup/DEVS_Setup/F_MAIN.cs
@@ -6,14 +6,181 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+
+using DEVS_Setup.Properties;
 
 namespace DEVS_Setup
 {
     public partial class F_MAIN : Form
     {
+        static int fileNumber = 0;
+        static int directoryNumber = 0;
+
+        string INISaveFilepath = Application.StartupPath + @"\DEVSinfo.ini";
+
         public F_MAIN()
         {
             InitializeComponent();
         }
+
+        private void F_MAIN_Load(object sender, EventArgs e)
+        {
+            FileStream fs;
+            StreamWriter sw;
+
+            try
+            {
+                using (fs = new FileStream(INISaveFilepath, FileMode.OpenOrCreate))
+                {
+                    using (sw = new StreamWriter(fs))
+                    {
+                        sw.WriteLine("");
+                    }
+                }
+            }
+            catch
+            {
+                
+            }                                                                           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileStream fs;
+            StreamReader sr;
+
+            try
+            {
+                using (fs = new FileStream(INISaveFilepath, FileMode.Open))
+                {
+                    using (sr = new StreamReader(fs))
+                    {
+
+                    }
+                }
+
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.FileName = "";
+                ofd.DefaultExt = "exe";
+                ofd.Filter = "exe Files (*.exe)|*.exe";
+
+                if (DialogResult.OK == ofd.ShowDialog())
+                {
+                    textBox1.Text = ofd.FileName;
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (DialogResult.OK == fbd.ShowDialog())
+            {
+                textBox2.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            
+            ofd.FileName = "";
+            ofd.DefaultExt = "exe";
+            ofd.Filter = "exe Files (*.exe)|*.exe";
+
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                textBox3.Text = ofd.FileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string MainPath = Settings.Default["MainPath"].ToString();
+
+            string firstPath = Settings.Default["SESEditorPath"].ToString();
+            string secondPath = Settings.Default["DEVSObjectPath"].ToString();
+            string thirdPath = Settings.Default["DEVSDiagramDisplayPath"].ToString();
+
+            DirectoryInfo destDirectory;
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    destDirectory = new DirectoryInfo(MainPath);
+
+                    if (!destDirectory.Exists)
+                    {
+                        destDirectory.Create();
+                    }
+                    File.Copy(textBox1.Text, firstPath);
+
+                    fileNumber++;
+                }
+
+                if (textBox3.Text != "")
+                {
+                    destDirectory = new DirectoryInfo(MainPath);
+
+                    if (!destDirectory.Exists)
+                    {
+                        destDirectory.Create();
+                    }
+
+                    File.Copy(textBox3.Text, thirdPath);
+
+                    fileNumber++;
+                }
+
+                if (textBox2.Text != "")
+                {
+                    DirectoryInfo sourceDirectory = new DirectoryInfo(textBox2.Text);
+                    destDirectory = new DirectoryInfo(secondPath);
+
+                    if (!destDirectory.Exists)
+                    {
+                        destDirectory.Create();
+                    }
+
+                    CopyDirectories(sourceDirectory, destDirectory);
+                }
+
+                toolStripStatusLabel1.Text = string.Format("복사 완료, 총 {0}개의 디렉토리와 {1}개의 파일을 복사함", directoryNumber, fileNumber);
+            }
+            catch
+            {
+            }
+        }
+
+        #region 폴더 복사
+        private void CopyDirectories(DirectoryInfo sourceDirectory, DirectoryInfo destDirectory)
+        {
+            FileInfo[] sourceFiles = sourceDirectory.GetFiles();
+
+            foreach (FileInfo file in sourceFiles)
+            {
+                file.CopyTo(destDirectory.FullName + "\\" + file.Name);
+                fileNumber++;
+            }
+
+            DirectoryInfo[] sourceSubDirectories = sourceDirectory.GetDirectories();
+
+            foreach (DirectoryInfo subDirectory in sourceSubDirectories)
+            {
+                DirectoryInfo destSubDirectory = destDirectory.CreateSubdirectory(subDirectory.Name);
+
+                CopyDirectories(subDirectory, destSubDirectory);
+                directoryNumber++;
+            }
+
+        }
+        #endregion
     }
 }
